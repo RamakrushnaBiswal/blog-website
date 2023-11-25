2adaf7e1a341e4aeb96038f96500b792b76c9d8b
Ramakrushna Biswal <ramakrushnabunty@gmail.com>
2023-11-25 17:15:56 +0530

    added

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { connect } = require('http2');
+const path = require('path');
+const multer = require('multer');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -18,7 +21,8 @@ else{
 // Define Post schema
 const postSchema = new mongoose.Schema({
     title: String,
-    content: String
+    content: String,
+    image: String
 });
 
 const Post = mongoose.model('Post', postSchema);
@@ -38,9 +42,26 @@ app.get('/newpost', (req, res) => {
     res.render('newpost');
 });
 
-app.post('/newpost', async (req, res) => {
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, 'uploads'));
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+// ... (previous code)
+
+// Routes
+app.post('/newpost', upload.single('image'), async (req, res) => {
     const { title, content } = req.body;
-    const newPost = new Post({ title, content });
+    const image = req.file ? req.file.filename : ''; // Save the filename if an image was uploaded
+    const newPost = new Post({ title, content, image });
     await newPost.save();
     res.redirect('/');
 });
